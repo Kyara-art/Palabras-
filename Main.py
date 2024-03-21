e5fc6144665e2dcc831f1a3792cd13692a90bba1
Kyara-art <164271594+Kyara-art@users.noreply.github.com>
2024-03-20 19:18:22 -0500

    Update and rename README.md to Main.py
    agregar el codigo :D

---
+++ Main.py
@@ -0,0 +1,16 @@
+meme_dict - {
+  "CRINGE": "Algo excepcionalmente raroembarazoso",
+  "LOL": "Una respuesta común a algo gracioso",
+  "WTF": "Estar abrumado",
+  "NONONO": "Ligera desaprobación",
+  "CREEPY": "Aterrador, siniestro",
+  "ASH": "Ponerse agresivo/enojado"
+print("¡Bienvenido al Diccionario Palabras De memes!")
+for _ in range(5):
+    word = input("Escribepalabra que no entiendas (¡con mayúsculas!): ")
+    if wordmeme dict.keys():
+      print(meme_dict[palabra])
+    else:
+      print("Lo siento, no se encontró tu palabra en el diccionario.")
+      
+print("iGracias por usar el Diccionario Buen dia")
