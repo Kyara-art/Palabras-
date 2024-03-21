dfd44419076bad3a520d104c92a46e3b1994e6df
Kyara-art <164271594+Kyara-art@users.noreply.github.com>
2024-03-20 19:39:46 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,11 +5,13 @@ meme_dict - {
   "NONONO": "Ligera desaprobación",
   "CREEPY": "Aterrador, siniestro",
   "ASH": "Ponerse agresivo/enojado"
+}
 print("¡Bienvenido al Diccionario Palabras De memes!")
+
 for _ in range(5):
-    word = input("Escribepalabra que no entiendas (¡con mayúsculas!): ")
-    if wordmeme dict.keys():
-      print(meme_dict[palabra])
+    word = input("Escribe la palabra que no entiendas (¡con mayúsculas!): ")
+    if word in meme_dict.keys():
+      print(meme_dict[word])
     else:
       print("Lo siento, no se encontró tu palabra en el diccionario.")
       
