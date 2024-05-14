f45ff440bb72598ada4892dd15559b9d7a60389d
Kyara-art <164271594+Kyara-art@users.noreply.github.com>
2024-05-14 18:59:37 -0500

    Delete gen_password.py

--- gen_password.py
+++
@@ -1,9 +0,0 @@
-import random
-def gen(sho):
-
-    simbolos = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-    passw = "".join(random.choice(simbolos) for i in range(sho))
-    print("La clave generada es:", passw)
-    return passw
-cosax= int(input("Longitud clave: "))
-minji= gen(cosax)
